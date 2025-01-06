ff6d29093050996e81fd258428731f3a94f430b3
Josep Maria Vila <156566553+JosepMariaVila@users.noreply.github.com>
2025-01-06 12:37:53 +0100

    Update forwarder-hook.c

--- forwarder-hook.c
+++ forwarder-hook.c
@@ -1,13 +1,13 @@
 /**
  * This hook resend the amount from a payment received or a URI buy to the destinations saved in the namespace of the hooks account.
- * The hook accepts 10 destinations and it will divide the initial amount among the destinations.
+ * The hook accepts 20 destinations and it will divide the initial amount among the destinations.
  * You can add destinations to the namespaces
  * Hook Parameters to Add destinations using Invoke
  * ADD: Xahau Address transformed  from rAddress To Account ID. You can use https://hooks.services/tools/raddress-to-accountid or https://transia-rnd.github.io/xrpl-hex-visualizer/ . To check if you are doing it right, address: rBnGX5KRERL2vMtZU2hDpF4osbhvichmvn will be translated to 6E7FE2948037180F3646CC248FAF2BCACD59893C
- * NUM: The number to identify the address and be able to delete it (Only accepts from 00 to 09)
+ * NUM: The number to identify the address and be able to delete it (Only accepts from 00 to 19)
 
  * Hook Parameters to delete destinations using Invoke
- * DEL: Number (from 00 to 09)
+ * DEL: Number (from 00 to 19)
 
  You can check the status of your namespace visiting https://xahau-testnet.xrplwin.com/ checking your hook account
 
